610df61210be4de5eb8c104df567aa22a2acc770
florianackle <florianackle@gmail.com>
2025-02-13 09:59:02 +0100

    :tada: initial commit

---
+++ src/views/HomeView.vue
@@ -0,0 +1,12 @@
+<template>
+  <v-container>
+    <h1>Willkommen bei Flo4Fun!</h1>
+    <v-btn color="primary" @click="startGame">Spiel starten</v-btn>
+  </v-container>
+</template>
+
+<script setup lang="ts">
+const startGame = () => {
+  console.log("Spiel gestartet!");
+};
+</script>
