4b98c0fec3c540f2bb2d1ba0ac7ec28d84867623
florianackle <florianackle@gmail.com>
2025-02-28 10:54:28 +0100

    :sparkles: added vue3-toastify for better user experience

--- src/main.ts
+++ src/main.ts
@@ -4,5 +4,14 @@ import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
 import { createPinia } from 'pinia'
+import Vue3Toastify, { type ToastContainerOptions } from 'vue3-toastify'
+import 'vue3-toastify/dist/index.css'
 
-createApp(App).use(router).use(createPinia()).mount('#app')
+const app = createApp(App)
+app.use(router)
+app.use(createPinia())
+app.use(Vue3Toastify, {
+  autoClose: 3000,
+  theme: 'auto',
+} as ToastContainerOptions)
+app.mount('#app')
