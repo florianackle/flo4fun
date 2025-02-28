d93d9db5d64a7570bc7eed70289153c486485d22
florianackle <florianackle@gmail.com>
2025-02-28 15:53:26 +0100

    :alien: added route for PokemonView

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,11 +1,17 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import HomeView from '@/views/HomeView.vue'
+import PokemonView from '@/views/PokemonView.vue'
 
 const routes = [
   {
     path: '/',
     name: 'Home',
     component: HomeView,
+  },
+  {
+    path: '/pokemon',
+    name: 'PokemonView',
+    component: PokemonView,
   },
 ]
 
