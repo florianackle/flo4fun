ac08b7f02b549d8018c0175ea504ba033f91d27b
florianackle <florianackle@gmail.com>
2025-02-28 15:56:35 +0100

    :rotating_light: fixed eslint 'any' warning and create pokeAPIClient with baseURL

--- src/services/pokemonService.ts
+++ src/services/pokemonService.ts
@@ -1,21 +1,62 @@
-import apiClient from './apiService.ts'
+import { createApiClient } from './apiService'
+
+// Axios-Client for PokeAPI
+const pokeApiClient = createApiClient('https://pokeapi.co/api/v2')
+
+// Interfaces for PokeAPI data
+export interface OfficialArtwork {
+  front_default: string
+}
+
+export interface PokemonOtherSprites {
+  'official-artwork': OfficialArtwork
+}
+
+export interface PokemonSprites {
+  front_default: string
+  other: PokemonOtherSprites
+}
 
 export interface PokemonData {
   id: number
   name: string
-  sprites?: any
+  sprites: PokemonSprites
 }
 
+export interface PokemonName {
+  name: string
+  language: {
+    name: string
+    url: string
+  }
+}
+
+export interface PokemonSpeciesData {
+  id: number
+  names: PokemonName[]
+}
+
+/**
+ * Fetches Pokémon data by id
+ */
 export async function getPokemonById(id: number): Promise<PokemonData> {
-  const response = await apiClient.get(`/pokemon/${id}`)
+  const response = await pokeApiClient.get<PokemonData>(`/pokemon/${id}`)
   return response.data
 }
 
-export async function getPokemonSpeciesById(id: number): Promise<any> {
-  const response = await apiClient.get(`/pokemon-species/${id}`)
+/**
+ * Fetches Pokémon species data by id
+ * needed to get the German name (yes, for real...)
+ */
+export async function getPokemonSpeciesById(id: number): Promise<PokemonSpeciesData> {
+  const response = await pokeApiClient.get<PokemonSpeciesData>(`/pokemon-species/${id}`)
   return response.data
 }
 
+/**
+ * Generates a random Pokémon id
+ * 1st generation Pokémon are from 1 to 151
+ */
 export function getRandomPokemonId(): number {
   return Math.floor(Math.random() * 151) + 1
 }
